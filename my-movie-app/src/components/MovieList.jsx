f6dc7fee1eea8f16b9dced6d3d71cadc4bc1ef16
Emil Stjernlöf <emil.stjernlof@gmail.com>
2023-03-20 23:04:58 +0100

    Added som more css

--- my-movie-app/src/components/MovieList.jsx
+++ my-movie-app/src/components/MovieList.jsx
@@ -6,10 +6,9 @@ const MovieList = ({ filterMovies }) => {
   const movieList = filterMovies.filter((movie) => movie).map((movie) => (
     <li key={movie.imdbID}>
       <section className={myStyle.movieCard}>
-        <h3 className={myStyle.movieTitle}>{movie.Title}</h3>
-        <p className={myStyle.movieTitle}>
-          {movie.Year} / {movie.Type}
-        </p>
+        <h3 className={myStyle.movieTitle}>{movie.Title.length > 26 ? `${movie.Title.substring(0, 22)}...` : movie.Title}
+        </h3>
+          <p id={myStyle.yearType} className={myStyle.movieTitle}>{movie.Year} / {movie.Type}</p>
         <img src={movie.Poster} className={myStyle.poster} alt="" />
       </section>
     </li>
